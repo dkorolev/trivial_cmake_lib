537994b1002b3fc5496abfe597d6fb7707dd1918
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-12 20:37:26 +0200

    Added `TRIVIAL_CMAKE_LIB_ADD_TO_SUM`.

--- src/trivial_cmake_lib.cc
+++ src/trivial_cmake_lib.cc
@@ -1,5 +1,11 @@
 #include "trivial_cmake_lib.h"
 
+#ifndef TRIVIAL_CMAKE_LIB_ADD_TO_SUM
 int trivial_cmake_lib_add(int a, int b) {
   return a + b;
 }
+#else
+int trivial_cmake_lib_add(int a, int b) {
+  return a + b + TRIVIAL_CMAKE_LIB_ADD_TO_SUM;
+}
+#endif  // TRIVIAL_CMAKE_LIB_ADD_TO_SUM
